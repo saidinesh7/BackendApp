f0126dbb310a6a1925f4a4829eb1298f69d27cc8
saidinesh7 <saidinesh70367@gmail.com>
2024-11-10 15:04:38 +0530

    added server url for swagger

--- app/src/main/java/com/saloonapp/app/config/swagger/SwaggerConfig.java
+++ app/src/main/java/com/saloonapp/app/config/swagger/SwaggerConfig.java
@@ -6,14 +6,23 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(security = {@SecurityRequirement(name = "bearer-key")})
 public class SwaggerConfig {
+
    @Bean
    public OpenAPI swaggerApiConfig() {
+
+        Server server = new Server();
+    server.setUrl("https://merry-passion-production.up.railway.app");
+
        var info = new Info()
                .title("saloon API's")
                .description("saloon app apis")
@@ -24,6 +33,6 @@ public class SwaggerConfig {
                        .type(SecurityScheme.Type.HTTP)
                        .scheme("bearer")
                        .bearerFormat("JWT"));
-       return new OpenAPI().components(components).info(info);
+       return new OpenAPI().components(components).info(info).servers(List.of(server));
    }
 }
